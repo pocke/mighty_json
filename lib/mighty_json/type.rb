9d152aa2ca6760ccd605c23d1948cc544abcc1d2
Masataka Kuwabara <kuwabara@pocke.me>
2017-05-26 10:14:20 +0900

    Remove unused code
    The errors are defined in errors.rb

--- lib/mighty_json/type.rb
+++ lib/mighty_json/type.rb
@@ -196,46 +196,5 @@ module MightyJSON
         "enum(#{types.map(&:to_s).join(", ")})"
       end
     end
-
-    class UnexpectedFieldError < StandardError
-      attr_reader :path, :value
-
-      def initialize(path: , value:)
-        @path = path
-        @value = value
-      end
-
-      def to_s
-        position = "#{path.join('.')}"
-        "Unexpected field of #{position} (#{value})"
-      end
-    end
-
-    class IllegalTypeError < StandardError
-      attr_reader :type
-
-      def initialize(type:)
-        @type = type
-      end
-
-      def to_s
-        "#{type} can not be put on toplevel"
-      end
-    end
-
-    class Error < StandardError
-      attr_reader :path, :type, :value
-
-      def initialize(path:, type:, value:)
-        @path = path
-        @type = type
-        @value = value
-      end
-
-      def to_s
-        position = path.empty? ? "" : " at .#{path.join('.')}"
-        "Expected type of value #{value}#{position} is #{type}"
-      end
-    end
   end
 end
