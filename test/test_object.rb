74f31b00de2e91e11ae883ff23903abd413c58af
Masataka Kuwabara <kuwabara@pocke.me>
2017-05-27 10:48:36 +0900

    Optimize object type by skipping check unexpected filed error

--- test/test_object.rb
+++ test/test_object.rb
@@ -8,6 +8,7 @@ class TestObject < Minitest::Test
 
     assert{s.test.coerce(a: 1, b: 'foo') == {a: 1, b: 'foo'}}
     assert_raises(MightyJSON::Error){s.test.coerce(a: 'foo', b: 1)}
+    assert_raises(MightyJSON::Error){s.test.coerce(a: 1, c: 'bar')}
   end
 
   def test_object_with_ignored
@@ -31,5 +32,15 @@ class TestObject < Minitest::Test
     assert{s.test.coerce({b: 'foo'}) == {b: 'foo'}}
     assert{s.test.coerce({a: nil, b: 'foo'}) == {a: nil, b: 'foo'}}
     assert_raises(MightyJSON::Error){s.test.coerce(a: 'foo')}
+  end
+
+  def test_nested_object
+    s = MightyJSON.new do
+      let :test, object(a: numeric, b: object(foo: string, bar: number))
+    end
+
+    assert{s.test.coerce(a: 1, b: {foo: 'hoge', bar: 1}) == {a: 1, b: {foo: 'hoge', bar: 1}}}
+    ex = assert_raises(MightyJSON::UnexpectedFieldError){s.test.coerce(a: 1, b: {foo: 'hoge', bar: 1, baz: 2})}
+    assert{ex.message == 'Unexpected field of b.baz ({:foo=>"hoge", :bar=>1, :baz=>2})'}
   end
 end
